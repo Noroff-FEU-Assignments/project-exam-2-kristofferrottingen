3b47fc5323aec962a211c0aa67b70ae717051a47
KristofferRottingen <kriroe06@gmail.com>
2022-05-22 14:56:48 +0200

    responsive details

--- src/components/home/Sos.js
+++ src/components/home/Sos.js
@@ -6,14 +6,16 @@ function ShareOnSocials() {
   return (
     <div className='sos-section'>
         <div className='sos-right'>
-            <h2>Del på sosiale medier</h2>
-            <div className='line-sos'></div>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.</p>
-            <div className='sos-right-socials'>
-                <FontAwesomeIcon icon={faFacebookSquare} />
-                <FontAwesomeIcon icon={faInstagram} />
-                <FontAwesomeIcon icon={faTwitterSquare} />
-            </div>            
+            <div className='sos-right-content'>
+              <h2>Del på sosiale medier</h2>
+              <div className='line-sos'></div>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.</p>
+              <div className='sos-right-socials'>
+                  <FontAwesomeIcon icon={faFacebookSquare} />
+                  <FontAwesomeIcon icon={faInstagram} />
+                  <FontAwesomeIcon icon={faTwitterSquare} />
+              </div>            
+            </div>
         </div>
     </div>
   )
